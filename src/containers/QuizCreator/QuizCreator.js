b448069f8eee5b0fa03f24dddfac5ac6079942ea
ProWeb4All <proweb4all@narod.ru>
2019-12-15 22:36:52 +0300

    add CreateTest

--- src/containers/QuizCreator/QuizCreator.js
+++ src/containers/QuizCreator/QuizCreator.js
@@ -1,11 +1,40 @@
 import React, {Component} from 'react';
 import './QuizCreator.scss'
+import Button from '../../components/UI/Button/Button';
+import Input from '../../components/UI/Input/Input';
+
 
 export default class QuizCreator extends Component {
-  render() {
+    submitHandler = event => {
+      event.preventDefault()
+    }
+    addQuestionHandler = () => {}
+    createQuizHandler = () => {}
+    render() {
     return (
       <div className='quiz-creator'>
-          <h1>Quiz Creator</h1>
+        <div>
+          <h1>Создание теста</h1>
+          <form onSubmit={this.submitHandler}>
+            <Input />
+            <hr/>
+            <Input />
+            <Input />
+            <Input />
+            <Input />
+            <select></select>
+            <Button
+              type='primary'
+              onClick={this.addQuestionHandler}>
+                Добавить вопрос
+            </Button>
+            <Button
+              type='success'
+              onClick={this.createQuizHandler}>
+                Создать тест
+            </Button>
+          </form>
+        </div>
       </div>
     )
   }
